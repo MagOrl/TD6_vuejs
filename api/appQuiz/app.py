090577ba09b8e35cc7569f44bd0ccfe5b692b886
Revan <quentin.chaufour@etu.univ-orleans.fr>
2026-03-17 15:51:56 +0100

    chore: add API files

---
+++ api/appQuiz/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+def mkpath(p):
+    return os.path.normpath(
+        os.path.join(
+            os.path.dirname(__file__), p
+        )
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (f"sqlite:///{mkpath("../quiz.db")}")
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
