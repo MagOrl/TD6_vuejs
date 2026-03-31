05ce8806b5f2be5800eddb3d29b7ac8850ca2ee5
Your Name <magomed.arsamerzoev@etu.univ-orleans.fr>
2026-03-31 16:23:02 +0200

    refactoring + commencement de la connexion admin

--- TD6_quiz/src/main.js
+++ TD6_quiz/src/main.js
@@ -3,7 +3,6 @@ import './style.css'
 import {router} from './routeur.js'
 import App from './App.vue'
 
-import { router } from './router/router.js'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
