0dcb8a47136d8230a4c265bcbb6b1010d0e7f335
Your Name <magomed.arsamerzoev@etu.univ-orleans.fr>
2026-03-31 16:24:11 +0200

    refactoring + commencement de la connexion admin

---
+++ TD6_quiz/src/components/Home.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+<template>
+    <div class="container">
+        <div class="section">
+            <h3>Connexion admin</h3>
+            <button>Se connecter</button>
+        </div>
+        <div class="section">
+            <h3>Faire le quizz</h3>
+            <button @click="$router.push('/quiz/')"> Jouer</button>
+        </div>
+    </div>
+
+</template>
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+}
+
+.section {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+</style>
